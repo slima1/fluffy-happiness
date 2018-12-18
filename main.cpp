a7571af5f9d58c8e3145f20a78f93c099e6b6fc9
slima <slima@btslivh.eu>
2018-12-18 16:50:28 +0100

    Ajout d'une phrase devenu culte de RRRrrrr!!!

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g1
+ *
+ * Created on 18 décembre 2018, 16:47
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << " Il va faire tout noir..." << endl;
+    return 0;
+}
+
